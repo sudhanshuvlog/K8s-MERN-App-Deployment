db970f950c01908fe5a262106751f36755ce0621
Sudhanshu Pandey <57751726+sudhanshuvlog@users.noreply.github.com>
2024-09-08 18:19:37 +0530

    Create CommandHistoryEKS-Statefulset.sh

---
+++ CommandHistoryEKS-Statefulset.sh
@@ -0,0 +1,50 @@
+yum install git -y 
+git clone https://github.com/sudhanshuvlog/K8s-MERN-App-Deployment.git
+git switch dev
+aws configure
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl version --short --client
+
+
+eksctl create cluster --name=EKS-1 \
+                      --region=ap-south-1 \
+                      --zones=ap-south-1a,ap-south-1b \
+                      --without-nodegroup
+
+eksctl utils associate-iam-oidc-provider \
+    --region ap-south-1 \
+    --cluster EKS-1 \
+    --approve
+
+eksctl create nodegroup --cluster=EKS-1 \
+                       --region=ap-south-1 \
+                       --name=node2 \
+                       --node-type=t3.medium \
+                       --nodes=2 \
+                       --nodes-min=2 \
+                       --nodes-max=2 \
+                       --node-volume-size=20 \
+                       --ssh-access \
+                       --ssh-public-key=DevOps \
+                       --managed \
+                       --asg-access \
+                       --external-dns-access \
+                       --full-ecr-access \
+                       --appmesh-access \
+                       --alb-ingress-access
+
+kubectl -n kube-system describe configmap aws-auth
+go to the role and attach the policy
+
+kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/ecr/?ref=release-1.4"
+
+kubectl apply -f secret.yml
+run the service account and then mongo-app
+
+
